6d4335cf496d89caa4b44def8d2ca916a0ad983e
Harshit Jain <harshitostwal1234@gmail.com>
2026-05-13 13:35:41 +0530

    refactor(ui/breadcrumb): update import to @shery-ui/utils

--- packages/components/ui/breadcrumb.jsx
+++ packages/components/ui/breadcrumb.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { Slot } from "@radix-ui/react-slot";
-import { cn } from "@repo/utils";
+import { cn } from "@shery-ui/utils";
 import { ChevronRight, MoreHorizontal } from "lucide-react";
 
 function Breadcrumb(props) {
